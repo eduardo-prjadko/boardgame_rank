2f91e0c998c098dadc1d5e42f7a3301772484ca2
Eduardo Prjadko <eduardoabp@gmail.com>
2025-06-03 21:57:57 +0000

    feat(app): add regoster page

--- app/src/bg_rank/login/views.py
+++ app/src/bg_rank/login/views.py
@@ -4,3 +4,7 @@ from django.http import HttpRequest, HttpResponse
 
 def login(request: HttpRequest) -> HttpResponse:
     return render(request, "login/login.html")
+
+
+def register(request: HttpRequest) -> HttpResponse:
+    return render(request, "login/register.html")
