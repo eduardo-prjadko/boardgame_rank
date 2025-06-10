2b8ad352fb6874d3614ba5751b27b3bbc3d75171
Eduardo Prjadko <eduardoabp@gmail.com>
2025-06-10 21:36:08 +0000

    fea(app): add many-to-many relationships

--- app/src/bg_rank/dashboard/admin.py
+++ app/src/bg_rank/dashboard/admin.py
@@ -1,18 +1,30 @@
 from django.contrib import admin
 
-from .models import Season, Match, Boardgame
+from .models import Season, Match, Boardgame, MatchPlayer, SeasonPlayer
 
 
 class BoardgameAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 
+class SeasonPlayerInLine(admin.TabularInline):
+    model = SeasonPlayer
+    extra = 1
+
+
 class SeasonAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+    inlines = [SeasonPlayerInLine]
+
+
+class MatchPlayerInLine(admin.TabularInline):
+    model = MatchPlayer
+    extra = 1
 
 
 class MatchAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+    inlines = [MatchPlayerInLine]
 
 
 admin.site.register(Boardgame, BoardgameAdmin)
--- app/src/bg_rank/dashboard/models.py
+++ app/src/bg_rank/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Boardgame(models.Model):
@@ -14,6 +15,7 @@ class Boardgame(models.Model):
 class Season(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField()
+    enrolled_players = models.ManyToManyField(User, through="SeasonPlayer")
     start_at = models.DateField()
     end_at = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -30,6 +32,7 @@ class Match(models.Model):
     season_id = models.ForeignKey(
         Season, on_delete=models.SET_NULL, null=True, blank=True
     )
+    enrolled_players = models.ManyToManyField(User, through="MatchPlayer")
     is_friendly = models.BooleanField(default=True)
     is_finished = models.BooleanField(default=False)
     start_at = models.DateTimeField()
@@ -39,3 +42,50 @@ class Match(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class MatchPlayer(models.Model):
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    score = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["player", "match"], name="unique_match_player"
+            )
+        ]
+
+
+class SeasonPlayer(models.Model):
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
+
+    @property
+    def score(self):
+        return (
+            MatchPlayer.objects.filter(
+                player=self.player, match__season_id=self.season
+            ).aggregate(total=models.Sum("score"))["total"]
+            or 0.0
+        )
+
+    @property
+    def average_score(self):
+        result = MatchPlayer.objects.filter(
+            player=self.player, match__season_id=self.season
+        ).aggregate(avg=models.Avg("score"))["avg"]
+        return result or 0.0
+
+    @property
+    def match_count(self):
+        return MatchPlayer.objects.filter(
+            player=self.player, match__season_id=self.season
+        ).count()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["player", "season"], name="unique_season_player"
+            )
+        ]
